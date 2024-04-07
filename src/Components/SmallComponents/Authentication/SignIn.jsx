20a20ddd81ce6b882f4c90d22637b4dbd6168cad
oindil <oindilgolder303@gmail.com>
2024-04-07 23:12:14 +0530

    [added]

--- src/Components/SmallComponents/Authentication/SignIn.jsx
+++ src/Components/SmallComponents/Authentication/SignIn.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import {auth} from "../../Firebase/firebase"
 
 function SignIn() {
+  // states
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+
+  const handleSubmit = () =>{
+    // todo
+  }
   return (
     <div>
-      <form className='container' style={{maxWidth:'600px',marginTop:'70px'}}>
+      <form className='container' onSubmit={handleSubmit} style={{maxWidth:'600px',marginTop:'70px'}}>
       <div className="mb-3">
         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='mail@mail.com'/>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='mail@mail.com' onChange={(e)=>setEmail(e.target.value)}/>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Enter Your Password'/>
+        <input type="current-password" className="form-control" id="exampleInputPassword1" placeholder='Enter Your Password' onChange={(e)=>setPassword(e.target.value)}/>
       </div>
       <button type="submit" className="btn btn-success">Sign In</button>
     </form>
--- src/Components/SmallComponents/Authentication/SignUp.jsx
+++ src/Components/SmallComponents/Authentication/SignUp.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function SignUp() {
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        // todo
+    }
     return (
         <div>
-            <form className='container' style={{ maxWidth: '600px',marginTop:'70px' }}>
+            <form className='container' onSubmit={handleSubmit} style={{ maxWidth: '600px', marginTop: '70px' }}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Email' />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Email' onChange={(e)=>setEmail(e.target.value)}/>
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="exampleInputPassword1" placeholder='Enter Your Name'/>
+                    <label htmlFor="exampleInputName1" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="exampleInputName1" placeholder='Enter Your Name'
+                    onChange={(e)=>setName(e.target.value)}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Create A Strong Password'/>
+                    <input type="current-password" className="form-control" id="exampleInputPassword1" placeholder='Create A Strong Password' onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <button type="submit" className="btn btn-primary">Sign Up</button>
             </form>
