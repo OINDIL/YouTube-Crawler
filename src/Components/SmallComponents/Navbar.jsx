4a45b1aabf498e0da60c40b5c9b910147a4e6907
oindil <oindilgolder303@gmail.com>
2024-04-23 12:31:40 +0530

    [build]

--- src/Components/SmallComponents/Navbar.jsx
+++ src/Components/SmallComponents/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <Link className='navbar-brand' to="/" onClick={() => handleActive('channles')}>Biswas Crawler</Link>
+                    <Link className='navbar-brand' to="/" onClick={() => handleActive('channles')}>YT CRAWLER</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
